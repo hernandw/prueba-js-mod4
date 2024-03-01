6dd106c40dd1f7ae72cfb7ed022ecd867a12d346
Williams Hernandez <hernandw@gmail.com>
2024-03-01 14:40:10 -0300

    :sparkles: eventos change para cambiar la imagen

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,32 @@
 import { Leon, Lobo, Oso, Serpiente, Aguila } from "./modulo/tipo.js";
+import animalesData from "./modulo/Animales.js";
+
+const animalSelect = document.getElementById("animal");
+const edadSelect = document.getElementById("edad");
+const comentarios = document.getElementById("comentarios");
+const preview = document.getElementById("preview");
+const btnRegistrar = document.getElementById("btnRegistrar");
+const animalesCard = document.getElementById("Animales");
+
+let imageSrc;
+let sonido;
+let animales = [];
+
+//agregar el evento para cambiar la imagen del preview
+
+animalSelect.addEventListener("change", async () => {
+  const animal = animalSelect.value;
+  const animales = await animalesData.getData();
+  const animalData = animales.find((a) => a.name === animal);
+  console.log(animalData);
+  imageSrc = `./assets/imgs/${animalData.imagen}`;
+  sonido = `./assets/sounds/${animalData.sonido}`;
+
+  preview.style.backgroundImage = `url(${imageSrc})` 
+});
+
+//agergar el evento que me permita agregar los animales
+
+btnRegistrar.addEventListener("click", () => {
+    
+})
---
+++ assets/js/modulo/Animales.js
@@ -0,0 +1,19 @@
+
+
+const animales = (() => {
+    
+const getData = async () => {
+  const response = await fetch("../../../animales.json");
+  const { animales } = await response.json();
+
+  return animales;
+};
+
+
+    return {
+        getData
+
+    }
+})()
+
+export default animales
